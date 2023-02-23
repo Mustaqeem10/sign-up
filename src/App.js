23b523a93dc0340e00fa62d10ab6605c2c7c9ade
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-02-23 11:17:17 +0500

    sign up form completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [formData, setFormData] = useState(
+        {
+            email: "",
+            password: "",
+            confirmPassword: "",
+            isJoined: false
+        }
+    )
+
+    function handleChange(event) {
+        const {name, type, value, checked} = event.target
+        setFormData((prevData) => {
+            return (
+                {
+                    ...prevData,
+                    [name]: type === "checkbox" ? checked : value
+                }
+            )
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        if (formData.password === formData.confirmPassword){
+            if (formData.isJoined){
+                console.log("Sign Up Successful \nThanks for signing up for our newsletter!")
+            }
+            else{
+                console.log("Sign Up Successfull")
+            }
+        }
+        else {
+            console.log("Passwords do not match")
+        }
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <input
+                type='email'
+                placeholder='Email' 
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <input
+                type='password'
+                placeholder='Password' 
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <input
+                type='password'
+                placeholder='Confirm Password' 
+                name='confirmPassword'
+                value={formData.confirmPassword}
+                onChange={handleChange}
+            />
+            <div className='checkbox-container'>
+            <input
+                id='isJoined'
+                type='checkbox'
+                name='isJoined'
+                checked={formData.isJoined}
+                onChange={handleChange}
+            />
+            <label 
+            htmlFor='isJoined'>
+                I want to join the newsletter
+            </label>
+            </div>
+            <button>Sign up</button>
+        </form>
+        </>
+    )
 }
-
-export default App;
